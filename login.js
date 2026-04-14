2c52bc7bb4784cfaadc144d2fc37ee4538f16291
unknown <kishorekrish69118@gmail.com>
2026-04-14 22:26:33 +0530

    login page added

---
+++ login.js
@@ -0,0 +1,28 @@
+
+document.getElementById("loginForm").addEventListener("submit", function(e) {
+  e.preventDefault();
+
+  let email = document.getElementById("email").value.trim();
+  let password = document.getElementById("password").value.trim();
+  let errorMsg = document.getElementById("errorMsg");
+
+  // Email regex check
+  let emailPattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
+
+  if (emailPattern.test(email) && password.length >= 6) {
+
+    // ✅ correct input
+    errorMsg.classList.add("hidden");
+
+    alert("REGISTRATION SUCCESSFULLY ✅");
+
+    // redirect
+    window.location.href = "index.html";
+
+  } else {
+
+    // ❌ wrong input
+    errorMsg.classList.remove("hidden");
+
+  }
+});
